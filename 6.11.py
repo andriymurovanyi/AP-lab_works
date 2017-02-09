79782ad0cc81130bc373de75d4af8bbf4222695d
andriymurovanyi <andriymurovanyi@gmail.com>
2017-02-09 21:12:23 +0200

    Add files via upload

--- 6.11.py
+++ 6.11.py
@@ -8,21 +8,24 @@ while flag:
         d, m, y = int(input('Day: ')), int(input('Mounth: ')), int(input('Year: '))
     except ValueError:
         print('Day must be integer number')
-    if d in days and y in years and m in mounths:
-        if d in range(1, 31):
-            print('Date of the next day: {}.{}.{}'.format(d + 1, m, y))
-        elif d == 31 or m == 12:
-            print('Date of the next day: {}.{}.{}'.format(1, 1, y + 1))
-        elif d == 31:
-            print('Date of the next day: {}.{}.{}'.format(1, m + 1, y))
     else:
-        print('Data out of range!')
-
+        if d in days and m in mounths and y in years:
+            if d in range(1, 31):
+                print('Date of the next day: {}.{}.{}'.format(d + 1, m, y))
+            elif d + 1 == 32:
+                d = 1
+                if m + 1 == 13:
+                    m = 1
+                    y += 1
+                    print('Date of the next day: {}.{}.{}'.format(d, m, y))
+                else:
+                    m += 1
+                    print('Date of the next day: {}.{}.{}'.format(d, m , y))
+        else:
+            print('Data out of range!')
     while True:
         x = input('Try again? [y/n]').lower()
         if x == 'y':
             break
         elif x == 'n':
             sys.exit()
-
-
