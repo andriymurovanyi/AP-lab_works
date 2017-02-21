23767c700035424ba8e13756ae89f3a9c9d5898e
andriymurovanyi <andriymurovanyi@gmail.com>
2017-02-21 04:20:56 +0200

    Done!

--- Lab_work_7/7.2.py
+++ Lab_work_7/7.2.py
@@ -1,18 +1,21 @@
 import re
 letters = {'к', 'п', 'т', 'с', 'ф', 'х', 'ц', 'ч', 'ш'}
 
-flag = True
-while flag:
-    c = 0
+while True:
     sent = input('Введите предложение (через запятую) и поставьте в конце точку: ')
-    a = set(sent)
-    if ',' not in sent or not sent.endswith('.'):
-        print('Проверьте знаки препинания!')
+    if ',' not in sent:
+        print('Слова должны быть через запятую!')
+        continue
     else:
-        if not re.search('[А-Яа-я]', sent):
-            print('Должны быть русскоязычные слова!')
+        if not sent.endswith('.'):
+            print('Поставьте в конце точку!')
+            continue
         else:
-            wrd = sent.split(',')
-            print(sorted(letters - a))
+            if not re.search('[А-Яа-я]', sent):
+                print('Должны быть русскоязычные слова!')
+            else:
+                sent.replace('.', '')
+                a = set(sent)
+                print(set(sorted(letters - a)))
     if input('Tap Enter key to continue...') != '':
         break
