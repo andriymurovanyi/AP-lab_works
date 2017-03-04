513340f2d3af814679571ebee07bac52387aceb2
andriymurovanyi <andriymurovanyi@gmail.com>
2017-03-04 03:51:47 +0200

    Update Lab_8(11).py

--- Lab_work_8/Lab_8(11).py
+++ Lab_work_8/Lab_8(11).py
@@ -32,10 +32,6 @@ while True:
         print('Результат {}'.format(np.roll(b, -k)))
     # Произведение двух матриц.
     elif ans == '3':
-        print('Введите размерность!')
-        b = np.matrix(np.zeros((int(input()), int(input()))), dtype=int)
-
-        print(b)
         pass
     # Определитель матрицы.
     elif ans == '4':
