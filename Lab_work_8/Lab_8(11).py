4536cb83a681da0c6e663a21a9327552f35f6330
andriymurovanyi <andriymurovanyi@gmail.com>
2017-03-16 14:15:16 +0200

    optimized.

--- Lab_work_8/Lab_8(11).py
+++ Lab_work_8/Lab_8(11).py
@@ -1,4 +1,20 @@
 import numpy as np
+
+
+def matrix():  # Инициализируем массив для его дальнейшего использования.
+    shape = input('Размерность: ').split('x')
+    n = int(shape[0])
+    m = int(shape[1])
+    global a, b
+    a = np.zeros((n, m), dtype=int)
+    b = np.zeros((n, m), dtype=int)
+    for i in range(n):
+        for j in range(m):
+            a[i, j] = int(input('a[' + str(i) +
+                                '][' + str(j) + '] = '))
+            if ans == '3':
+                b[i, j] = int(input('b[' + str(i) +
+                                    '][' + str(j) + '] = '))
 while True:
     print('Выберите, что хотите сделать: ')
     ans = input('[1 - Повернуть массив]\n'
@@ -7,48 +23,27 @@ while True:
                 '[4 - Вычислить определитель]\nЯ:')
     # Поворот на 90 градусов.
     if ans == '1':
-        try:
-            shape = input('Введите размерность:').split('x')
-            n = m = int(shape[0])
-            a = np.zeros((n, m), dtype=int)
-        except (IndexError, ValueError):
-            print('Введите размерность корректно!')
-        else:
-            for i in range(n):
-                for j in range(m):
-                    a[i, j] = int(input('a[' + str(i) + ']'
-                                        '[' + str(j) + '] = '))
-            print(np.rot90(a, 3), sep='\t')
+        matrix()
+        print('Начальный массив: \n', np.matrix(a))
+        print('Результирующий массив:\n', np.rot90(a, -1))
     # Сдвиг на К елементов.
     elif ans == '2':
         lenght = int(input('Введите длинну массива: '))
         b = np.zeros(lenght, dtype=int)
         for i in range(lenght):
             b[i] = int(input('b[' + str(i) + '] = '))
-        k = int(input('На сколько позиций сдвинуть? \n'))
+        k = int(input('На сколько позиций сдвинуть? \nЯ:'))
         print('Результат {}'.format(np.roll(b, -k)))
     # Произведение двух матриц.
     elif ans == '3':
-        m = n = int(input('Введите размерность:'))
-        a = b = np.zeros((n, m), dtype=int)
-        for i in range(n):
-            for j in range(m):
-                a[i, j] = a[i, j] = int(input('a[' + str(i) +
-                                              '][' + str(j) + '] = '))
-                print('Mатрица 1: \n', a)
-                b[i, j] = a[i, j] = int(input('b[' + str(i) +
-                                              '][' + str(j) + '] = '))
-                print('Матрица 2: \n', b)
-        print(np.matrix(np.dot(np.matrix(a), np.matrix(b))))
+        matrix()
+        print('Матрица 1:\n', np.matrix(a))
+        print('Матрица 2: \n', np.matrix(b))
+        print('Вот, что получилось:\n',np.matrix(np.dot(np.matrix(a),
+                                                        np.matrix(b))))
     # Определитель матрицы.
     elif ans == '4':
-        n = m = int(input('Размерность: '))
-        a = np.zeros((n, m), dtype=int)
-        for i in range(n):
-            for j in range(m):
-                a[i, j] = int(input('a[' + str(i) + '][' + str(j) + '] = '))
-        print('Определитель{}'.format(np.linalg.det(a)))
-
-
-
-
+        matrix()
+        print('Определитель: {}'.format(int(np.linalg.det(a))))
+    if input('Чтобы продолжить нажмите Enter...') != '':
+        break
