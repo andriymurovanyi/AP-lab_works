bc2b10f612ed8c76f1f0e9d6914c2cfb4dfa54bb
andriymurovanyi <andriymurovanyi@gmail.com>
2017-03-30 02:37:22 +0300

    Add files via upload

---
+++ WF_algorithm.py
@@ -0,0 +1,16 @@
+haystack = input('Введите строку, в которой искать: ')
+needle = input('Искомый шаблон: ')
+m = len(haystack)
+n = len(needle)
+d = [[0 for i in range(n + 1)] for j in range(m + 1)]
+for i in range(m + 1):
+    d[i][0] = i
+for j in range(n + 1):
+    d[0][j] = j
+for i in range(1, m + 1):
+    for j in range(1, n + 1):
+        if haystack[i - 1] == needle[j - 1]:
+            d[i][j] = d[i - 1][j - 1]
+        else:
+            d[i][j] = 1 + min(d[i - 1][j], d[i - 1][j - 1], d[i][j - 1])
+print(d[m][n])
